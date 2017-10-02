d816ab9ae303a6115d8640f47e3548caab7a7b04
Navi Mann <navdeep.k.mann@gmail.com>
2017-10-02 15:13:34 -0400

    added event listener to switch light on and light off

--- js/main.js
+++ js/main.js
@@ -1,2 +1,22 @@
+document.addEventListener('DOMContentLoaded', function() {
+
+var body = document.querySelector('body');
+var button = document.querySelector('button');
+var text = document.querySelector('h1')
 
 
+button.addEventListener('click',function () {
+    if(button.className === "switch on") {
+    button.className = "switch off";
+    body.className= 'dark'
+    text.innerText = "Hey, who turned off the lights?";
+  } else if (button.className === "switch off") {
+    button.className = 'switch on';
+    body.className = 'light';
+    text.innerText = "It's so bright in here!";
+
+  };
+
+});
+
+});
